d70b17636a67c0568b005752e923accfa7668a9e
Joeri Hermans <joeri@joerihermans.com>
2019-03-21 15:25:40 +0100

    First simulator

---
+++ lfibenchmark/linear/simulator.py
@@ -0,0 +1,24 @@
+import torch
+
+
+
+class Simulator(lfibenchmark.simulator.Simulator):
+
+    def __init__(self):
+        super(Simulator, self).__init__()
+
+    def forward(self, inputs):
+        inputs = inputs.view(-1, 3)
+        num_samples = inputs.size(0)
+        # Fetch the seperate parameters.
+        m = inputs[:, 0]
+        b = inputs[:, 1]
+        f = inputs[:, 2]
+        x = 10 * torch.rand(num_samples)
+        y_err = .1 + .5 * torch.rand(num_samples)
+        y = m * x + b
+        y += (f * y).abs() * torch.rand(num_samples)
+        y += y_err * torch.rand(num_samples)
+        outputs = torch.cat([x, y, y_err], dim=1)
+
+        return outputs
---
+++ lfibenchmark/simulator.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+
+
+
+class Simulator(torch.nn.Module):
+
+    def __init__(self):
+        super(Simulator, self).__init__()
+
+    def forward(self, inputs):
+        raise NotImplementedError
+
+    def terminate(self):
+        pass
